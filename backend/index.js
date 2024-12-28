6b2703f62db02fa6006e24c503e7687a8d4ab862
SagnikSaha01 <sahasagnik999@gmail.com>
2024-12-27 20:36:45 -0500

    fixed spotify song functionality

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import songRoutes from './routes/songRoutes.js';
@@ -8,13 +7,6 @@ import session from 'express-session';
 import dotenv from 'dotenv';        // if not installed, do `npm i dotenv`
 import connectPgSimple from 'connect-pg-simple';
 import pkg from 'pg';
-=======
-const { OpenAI } = require('openai');
-const express = require('express');
-const {login, createPlaylist, logout} = require('./spotify.js');
-const path = require('path');
-require('dotenv').config();
->>>>>>> Stashed changes
 
 dotenv.config();
 
@@ -22,24 +14,13 @@ const {Pool} = pkg;
 const pgSession = connectPgSimple(session);
 
 const app = express();
-<<<<<<< Updated upstream
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for x-www-form-urlencoded bodies
-=======
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
-
-app.listen(port, console.log('Server started on port: ' + port));
->>>>>>> Stashed changes
 
 login(app);
 createPlaylist(app);
 logout(app);
 
-<<<<<<< Updated upstream
 // session store Pool
 const dbPool = new Pool({
   user: 'postgres',
@@ -79,20 +60,3 @@ app.use('/', songRoutes);
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
-=======
-/*
-async function testFunction() {
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { role: "user", content: "Give me some songs I may want to listen to if I am in a excited mood" }
-          ],
-          max_tokens: 30,
-    });
-    console.log(response.choices[0].message.content);
-}
-*/
-
-
-//testFunction();
->>>>>>> Stashed changes
